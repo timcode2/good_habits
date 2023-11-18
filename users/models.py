2d58529c2a839b2291fde81023b85ad0b46fc967
Artem Matrashov <artem.m2013@gmail.com>
2023-11-18 16:06:08 +0300

    add users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+    telegram_id = models.CharField(max_length=100, verbose_name='Телеграм ID пользователя', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
