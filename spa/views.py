71a8062db493ba7e9108945bd084ca151be3390c
Artem Matrashov <artem.m2013@gmail.com>
2023-11-19 22:01:45 +0300

    add views

--- spa/views.py
+++ spa/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from spa.models import Habit
+from spa.paginators import HabitPaginator
+from spa.serializers import HabitSerializer
+
+
+class HabitListAPIView(generics.ListAPIView):
+    """Класс-представление для отображения списка привычек текущего пользователя"""
+
+    serializer_class = HabitSerializer  # Сериализатор
+    permission_classes = [IsAuthenticated]  # Только для авторизованных пользователей
+    pagination_class = HabitPaginator  # пагинатор
+
+    def get_queryset(self):
+        """Фильтрация привычек текущего пользователя"""
+
+        queryset = Habit.objects.filter(user=self.request.user)
+        return queryset
+
+
+class PublicHabitListAPIView(generics.ListAPIView):
+    """Класс-представление для отображения списка всех публичных привычек на основе generics"""
+
+    serializer_class = HabitSerializer  # Сериализатор
+    queryset = Habit.objects.filter(is_public=True)  # фильтр
+    permission_classes = [IsAuthenticated]  # Только для авторизованных пользователей
+    pagination_class = HabitPaginator  # пагинатор
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    """Класс-представление для создания привычки на основе generics"""
+    serializer_class = HabitSerializer  # Сериализатор
+    permission_classes = [IsAuthenticated]  # Только для авторизованных пользователей
+
+    def perform_create(self, serializer):
+        """Добавление пользователя создаваемой привычке"""
+
+        new_habit = serializer.save()  # сохранение привычки
+        new_habit.user = self.request.user  # добавляем пользователя
+        new_habit.save()
+
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    """Класс-представление для обновления привычки на основе generics"""
+    serializer_class = HabitSerializer  # Сериализатор
+    permission_classes = [IsAuthenticated]  # Только для авторизованных пользователей
+
+    def get_queryset(self):
+        """Фильтрация привычек текущего пользователя"""
+        queryset = Habit.objects.filter(user=self.request.user)
+        return queryset
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    """Класс-представление для удаления привычки на основе generics"""
+    queryset = Habit.objects.all()  # Фильтр
+    permission_classes = [IsAuthenticated]  # Только для авторизованных пользователей
+
+    def get_queryset(self):
+        """Фильтрация привычек текущего пользователя"""
+
+        queryset = Habit.objects.filter(user=self.request.user)
+        return queryset
+
