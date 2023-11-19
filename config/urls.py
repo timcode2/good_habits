02000406ed30edf528a3251b582865b9fd0a12db
Artem Matrashov <artem.m2013@gmail.com>
2023-11-19 22:01:35 +0300

    add urls

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for config project.
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(openapi.Info(
+    title="Snippets API",
+    default_version='v1',
+    description="Test description",
+    terms_of_service="https://www.google.com/policies/terms/",
+    contact=openapi.Contact(email="contact@snippets.local"),
+    license=openapi.License(name="BSD License"
+                            ),
+),
+    public=True,
+    permission_classes=(permissions.AllowAny,),)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('spa.urls', namespace='spa')),
     path('users/', include('users.urls', namespace='users')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
