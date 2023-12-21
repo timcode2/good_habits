c7151254f6c2ec8f85dcee9de1645e2a1d139adf
tim <artem.m2013@gmail.com>
2023-12-21 23:23:54 +0300

    fix container work

--- config/settings.py
+++ config/settings.py
@@ -148,8 +148,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.AllowAny', ]
 }
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_BEAT_SCHEDULE = {'check_habits': {'task': 'spa.tasks.check_habits',
                         'schedule': timedelta(minutes=1)}}  # периодичность выполнения задачи 1 раз в минуту
 
